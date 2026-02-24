52c4ba6ca4f63a28a1f37e98d2596d9556a6b43e
Douglas Shanks <15614951+douglas-shanks@users.noreply.github.com>
2026-02-24 18:18:12 +0000

    Update himeno_omp.f90
    Fix OMP.

--- src/himeno_omp.f90
+++ src/himeno_omp.f90
@@ -218,11 +218,9 @@ DO loop=1,nn
   gosa=0.0
   wgosa=0.0
 
-!$omp target teams distribute reduction(+: wgosa) private(s0,ss) 
+!$omp target teams distribute parallel do collapse(3) reduction(+: wgosa) private(s0,ss) 
 DO k=2,kmax-1
-!$omp parallel do reduction (+: wgosa)
   DO j=2,jmax-1
-!$omp simd reduction (+: wgosa)
     DO i=2,imax-1
       s0=a(i,j,k,1)*p(i+1,j,k)+a(i,j,k,2)*p(i,j+1,k) +a(i,j,k,3)*p(i,j,k+1)  &
           +b(i,j,k,1)*(p(i+1,j+1,k)-p(i+1,j-1,k) -p(i-1,j+1,k)+p(i-1,j-1,k))  &
@@ -233,26 +231,20 @@ DO k=2,kmax-1
       ss=(s0*a(i,j,k,4)-p(i,j,k))*bnd(i,j,k)
       wgosa=wgosa+ss*ss
       wrk2(i,j,k)=p(i,j,k)+omega *ss
-    END DO
-!$omp end simd    
+    END DO  
   END DO
-!$omp end parallel do
 END DO
-!$omp end target teams distribute
+!$omp end target teams distribute parallel do
 
-!$omp target teams distribute
+!$omp target teams distribute parallel do collapse(3)
 DO k=2,kmax-1
-!$omp parallel do
   DO j=2,jmax-1
-!$omp simd  
     DO i=2,imax-1
       p(i,j,k)=wrk2(i,j,k)
     END DO
-!$omp end simd    
   END DO
-!$omp end parallel do
 END DO
-!$omp end target teams distribute
+!$omp end target teams distribute parallel do
 
 CALL sendp(ndx,ndy,ndz)
 
