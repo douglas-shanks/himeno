766fe3c43f7a8615a7b82d37d4276c040c502019
Douglas Shanks <15614951+douglas-shanks@users.noreply.github.com>
2026-02-25 13:20:06 +0000

    Update gpu_env.sh

--- run/gpu_env.sh
+++ run/gpu_env.sh
@@ -8,5 +8,4 @@ module load craype-x86-trento
 module load craype-accel-amd-gfx90a
 module load rocm
 # Required Environment settings
-export MPICH_GPU_SUPPORT_ENABLED=1
 export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
