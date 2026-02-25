37a48aece5667b17ffff9b2042183587d6245217
Douglas Shanks <15614951+douglas-shanks@users.noreply.github.com>
2026-02-25 13:18:34 +0000

    Update himeno_acc.f90 and ensure MPI standards compliance

--- src/himeno_acc.f90
+++ src/himeno_acc.f90
@@ -39,10 +39,10 @@
 ! -------------------
 
 PROGRAM himenobmtxp
+USE MPI
 
 IMPLICIT REAL*4(a-h,o-z)
 
-INCLUDE 'mpif.h'
 INCLUDE 'param.h'
 
 !     ttarget specifys the measuring period in sec
@@ -203,6 +203,8 @@ END SUBROUTINE initmt
 SUBROUTINE jacobi(nn,gosa)
 !*************************************************************
 
+USE MPI
+
 IMPLICIT REAL*4(a-h,o-z)
 
 INTEGER, INTENT(IN)                      :: nn
@@ -210,7 +212,6 @@ REAL, INTENT(OUT)                        :: gosa
 
 INTEGER*4 istat
 
-INCLUDE 'mpif.h'
 INCLUDE 'param.h'
 
 !$acc data copyin(a,b,c,p,wrk2,wrk1,bnd)
@@ -265,9 +266,10 @@ END SUBROUTINE jacobi
 
 SUBROUTINE initcomm
 
+USE MPI
+
 IMPLICIT REAL*4(a-h,o-z)
 
-INCLUDE 'mpif.h'
 INCLUDE 'param.h'
 
 LOGICAL :: ipd(3),ir
@@ -332,6 +334,8 @@ END SUBROUTINE initcomm
 
 SUBROUTINE initmax(mx,my,mz,ks)
 
+USE MPI
+
 IMPLICIT REAL*4(a-h,o-z)
 
 INTEGER, INTENT(IN)                      :: mx
@@ -340,7 +344,6 @@ INTEGER, INTENT(IN)                      :: mz
 INTEGER, INTENT(OUT)                     :: ks
 
 INCLUDE 'param.h'
-INCLUDE 'mpif.h'
 
 INTEGER :: itmp
 INTEGER :: mx1(0:ndx),my1(0:ndy),mz1(0:ndz)
@@ -458,9 +461,10 @@ END SUBROUTINE sendp
 
 SUBROUTINE sendp3()
 
+USE MPI
+
 IMPLICIT REAL*4(a-h,o-z)
 
-INCLUDE 'mpif.h'
 INCLUDE 'param.h'
 
 DIMENSION ist(mpi_status_size,0:3),ireq(0:3)
@@ -496,9 +500,10 @@ END SUBROUTINE sendp3
 
 SUBROUTINE sendp2()
 
+USE MPI
+
 IMPLICIT REAL*4(a-h,o-z)
 
-INCLUDE 'mpif.h'
 INCLUDE 'param.h'
 
 DIMENSION ist(mpi_status_size,0:3),ireq(0:3)
@@ -534,9 +539,10 @@ END SUBROUTINE sendp2
 
 SUBROUTINE sendp1()
 
+USE MPI
+
 IMPLICIT REAL*4(a-h,o-z)
 
-INCLUDE 'mpif.h'
 INCLUDE 'param.h'
 
 DIMENSION ist(mpi_status_size,0:3),ireq(0:3)
